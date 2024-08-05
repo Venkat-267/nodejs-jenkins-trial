e1cc3f924092454d7ffbf3fcca504c8f28d2e8e9
Venkata Krishnan R S <130453916+Venkat-267@users.noreply.github.com>
2024-08-05 22:11:24 +0530

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-    const { expect } = await import('chai');
-
-    describe('Sample Test', () => {
-        it('should return true', () => {
-            expect(true).to.be.true;
-        });
-    });
-})();
